a8bc97c443278f8601a7b81f19bd0c8e2cacb9bd
Handy Claude Marie Milliance <milliance23@gmail.com>
2024-10-22 19:07:07 -0300

    atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { TranslationService } from './service/translation.service';
 import { OucaComponent } from './components/pages/ouca/ouca.component';
@@ -11,18 +12,24 @@ import { FooterComponent } from './components/shared/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './components/pages/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     OucaComponent,
     FaleComponent,
+    HomeComponent,
     HeaderComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    MaterialModule,
+    RouterModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
